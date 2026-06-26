f66b90b8d073008967d15cb505e92242539ade96
Antheablz <agblais@icloud.com>
2026-06-25 20:01:41 -0700

    basic django project working

---
+++ product_catalog/product_catalog_app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
