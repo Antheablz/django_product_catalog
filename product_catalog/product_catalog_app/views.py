376d0de13a309011b034fdeff0a9eb7d1744a237
Antheablz <agblais@icloud.com>
2026-06-26 19:05:17 -0700

    displaying the products

--- product_catalog/product_catalog_app/views.py
+++ product_catalog/product_catalog_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+# def home(request):
+#     return render(request, 'index.html')
+
 def home(request):
-    return render(request, 'index.html')
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
