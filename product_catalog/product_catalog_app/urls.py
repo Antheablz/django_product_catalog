8b0bbbaa5f75fb339124115681c7ec721b8fc36f
Antheablz <agblais@icloud.com>
2026-06-27 12:22:05 -0700

    filtering and search work independently

--- product_catalog/product_catalog_app/urls.py
+++ product_catalog/product_catalog_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.product_page, name="product_page")
+    path("", views.home, name="home")
 ]
--- product_catalog/product_catalog_app/views.py
+++ product_catalog/product_catalog_app/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Category, Tag, Product
 
 # Create your views here.
-# def home(request):
-#     return render(request, 'index.html')
-
-def product_page(request):
+def home(request):
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
     products = Product.objects.all()
-    
-    return render(request, 'index.html', {'products': products})
+
+    search = request.GET.get("search")
+    category = request.GET.get("category")
+    tag = request.GET.get("tag")
+
+    if search:
+        products = products.filter(description__icontains=search)
+
+    if category:
+        products = products.filter(category__name=category)
+
+    if tag:
+        products = products.filter(tags__name=tag)
+
+    context = {
+        "categories": categories,
+        "tags": tags,
+        "products": products
+    }
+    return render(request, 'index.html', context)
