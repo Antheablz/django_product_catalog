dfe0618ce2071c42334b77b54c34d9c84e1a55a1
Antheablz <agblais@icloud.com>
2026-06-29 14:23:44 -0700

    adding doc strings

--- product_catalog/product_catalog_app/models.py
+++ product_catalog/product_catalog_app/models.py
@@ -2,18 +2,39 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
+    '''
+    Represents a category.
+
+    Attributes:
+        name (str): The name of the category.
+    '''
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 class Tag(models.Model):
+    '''
+    Represents a tag.
+
+    Attributes:
+        name (str): The name of the tag.
+    '''
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 class Product(models.Model):
+    '''
+    Represents a product.
+
+    Attributes:
+        name (str):             The name of the product.
+        description (str):      The product descroption.
+        category (Category):    What category the product as associated with
+        tags (QuerySet[Tag]):   The tags related to the product
+    '''
     name = models.CharField(max_length=100)
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
